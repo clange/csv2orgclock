0fc6f48305cc97fdedd8e57981f0a26d3dbc1a71
Christoph Lange <math.semantic.web@gmail.com>
2022-11-25 20:27:23 +0100

    parsing lines

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -2,15 +2,28 @@ import com.github.tototoshi.csv._
 
 /** Main program */
 @main def main: Unit = 
-  // Open CSV export file for reading
+  // Open CSV input file for reading
   val reader = CSVReader.open("Org.csv")
-  // read all rows from the CSV
-  reader.iteratorWithHeaders
-  // parse one CSV record to an Org tree
-    .foreach(
-      line => line.foreach {
-        case (key, value) =>
-          printf("%s -> %s\n", key, value)
-      }
-    )
+  // read header row from CSV
+  val headers = reader.readNext()
+  headers.map(
+    headers => {
+      // read all further rows from the CSV
+      reader.iterator.foreach(
+        line => {
+          // for each CSV record, create one Org tree
+          printf("* %s\n", line.head)
+          println("  :LOGBOOK:")
+          headers.tail.zip(line.tail).foreach(
+            (date, times) => {
+              if ! "".equals(times) then
+                printf("  CLOCK: ")
+                print("\n")
+              end if
+          )
+          println("  :END:")
+        }
+      )
+    }
+  )
   reader.close()
