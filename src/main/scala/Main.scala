cb97b74b3cf9e5e6566e716115d3078066b246b1
Christoph Lange <math.semantic.web@gmail.com>
2022-12-03 21:51:31 +0100

    full initial functionality (reverse output, difference, midnight adjustment)

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,6 +1,7 @@
 import com.github.tototoshi.csv._
 import java.time.LocalDate
 import java.time.format.{DateTimeFormatter,DateTimeFormatterBuilder}
+import java.time.temporal.ChronoUnit._
 
 val startEndHhMmPattern = "([0-9]{1,2})[:.]([0-9]{2})-(?:([0-9]{1,2})[:.])?([0-9]{2})".r
 
@@ -33,22 +34,26 @@ val dateTimeFormat = DateTimeFormatterBuilder()
   // read header row from CSV
   val headers = reader.readNext()
   headers.map( headers => {
+    val parsedReverseHeaders = headers.tail.reverse.map( date =>
+        LocalDate.parse(date, dateFormat)
+        )
     // read all further rows from the CSV
     reader.iterator.foreach( line => {
       // for each CSV record, create one Org tree
       printf("* %s\n", line.head)
       println("  :LOGBOOK:")
-      headers.tail.map( date =>
-          LocalDate.parse(date, dateFormat)
-          ).zip(line.tail).foreach( (date, intervals) => {
+      parsedReverseHeaders.zip(line.tail.reverse).foreach( (date, intervals) => {
             if ! "".equals(intervals) then
-              intervals.split(',').map(_.trim).foreach( interval => {
+              intervals.split(',').reverse.map(_.trim).foreach( interval => {
                 if !interval.isBlank then
                 splitHhMm(interval).map( (startHh, startMm, endHh, endMm) =>
                     val startDateTime = date.atTime(startHh, startMm)
-                    val endDateTime = date.atTime(endHh, endMm)
-                    // TODO handle post 24:00
-                    printf("  CLOCK: [%s]--[%s] => \n", startDateTime.format(dateTimeFormat), endDateTime.format(dateTimeFormat))
+                    val parsedEndDateTime = date.atTime(endHh, endMm)
+                    val endDateTime = if parsedEndDateTime.isBefore(startDateTime)
+                      then parsedEndDateTime.plusDays(1)
+                      else parsedEndDateTime
+                    val diffMinutes = MINUTES.between(startDateTime, endDateTime)
+                    printf("  CLOCK: [%s]--[%s] => %2d:%02d\n", startDateTime.format(dateTimeFormat), endDateTime.format(dateTimeFormat), diffMinutes / 60, diffMinutes % 60)
                     )
               })
             end if
